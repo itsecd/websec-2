c47b0b6a7f71231ed6e110cf2d9fbcbaf1b6b3ea
Dryg1214 <pakazancev901@gmail.com>
2022-12-08 18:30:04 +0400

    Готов поиск остановок

--- script.js
+++ script.js
@@ -46,18 +46,31 @@ async function load_stops_coord (){
     catch(err) { console.log('err:', err); }
 }
 
-function getMatches(searchStr, str) {
+//спросить здесь, был в параметре список
+async function getMatches(searchStr) {
+    console.log("зашел");
+    const data = await load_stops_coord();
+    
+    let stops_data = data.getElementsByTagName("stop");
+
     var ind = 0, searchStrL = searchStr.length;
     var index, matches = [];
 
-    str = str.toLowerCase();
     searchStr = searchStr.toLowerCase();
 
-    while ((index = str.indexOf(searchStr, ind)) > -1) {
-         matches.push(index);
-         ind = index + searchStrL;
+    for(let i = 0; i < stops_data.length; i++)
+    {
+        str = stops_data[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+        str = str.toLowerCase();
+        //console.log(stops_data[i].getElementsByTagName("KS_ID")[0].childNodes[0].nodeValue);
+        if ((index = str.indexOf(searchStr)) > -1) {
+            console.log(stops_data[i]);
+            matches.push(stops_data[i].getElementsByTagName("KS_ID")[0].childNodes[0].nodeValue);
+            //ind = index + searchStrL;
+        }
     }
-    return matches;
+    console.log(matches);
+    //return matches;
 }
 
 async function data_stops_coords()
@@ -66,22 +79,28 @@ async function data_stops_coords()
     console.log(data);
     let size = data.getElementsByTagName("stop").length;
     let stop_data = data.getElementsByTagName("stop");
-    let lst = new Array();
-    console.log(stop_data);
+    //let name = stop_data[0].getElementsByTagName("longitude")[0].childNodes[0].nodeValue;
+    //let name = stop_data[0].getElementsByTagName("latitude")[0].childNodes[0].nodeValue;
+    //let name = stop_data[0].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+    console.log(stop_data.length)
     
+    //let lst = new Array();
+    //console.log(name);
+    console.log(stop_data);
+
+    /*
     for (let i = 0; i < size; i++)
     {
         if (stop_data[i].childNodes[1].textContent == name_stop) {
             lst.push(data[i]);
         }
     }
+    */
     
-    if (lst.size === 0)
-    {
-        lst = "No Matches";
-    }
+    return stop_data
 }
 
 
 
-let ab = data_stops_coords();
+//let ab = data_stops_coords();
+let ss = getMatches("гагарина");
