3ae9145638200e10157e1f13aeb161864260928c
Santas7 <andreypanyshkin03@gmail.com>
2025-03-19 18:24:48 +0400

    добавлен Header и его составляющие

--- app/src/components/header/Header.jsx
+++ app/src/components/header/Header.jsx
@@ -1,5 +1,41 @@
+import { Burger, Container, Group } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import styles from './Header.module.scss';
+import LinkItem from './link-item/LinkItem';
+import AppName from './app-name/AppName';
+
+const links = [
+  { link: '/', label: 'Главная' },
+  { link: '/schedule', label: 'Расписание' },
+  { link: '/favorites', label: 'Избранное' },
+  { link: '/map', label: 'Карта' },
+];
+
 export default function Header() {
-    return (
-        <h1>header</h1>
-    )
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const items = links.map((link) => (
+    <LinkItem link={link} styles={styles} />
+  ));
+
+  return (
+    <header className={styles.header}>
+      <Container size="md">
+        <div className={styles.inner}>
+          <AppName styles={styles}/>
+          <Group gap={5} visibleFrom="sm" className={styles.nav}>
+            {items}
+          </Group>
+          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+          {opened && (
+            <div className={styles.mobileMenu}>
+              <Group gap={5} direction="column">
+                {items}
+              </Group>
+            </div>
+          )}
+        </div>
+      </Container>
+    </header>
+  );
 }
